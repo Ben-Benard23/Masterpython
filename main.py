7cba70cd36882bd5acba07dd9bdd72351fb9afed
“[Benard <ngwabern@gmail.com>
2024-10-05 12:53:59 -0500

    variables and data types added

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-print("Hello class")
-a=1
-print(a)
-print("a")
+# print("Hello class")
+
+# x = "Ben"
+# print(x)
+# make your variables descriptive
+# name = "Ben"
+# print(name)
+
+# Data types( what type of data the variable holds)
+# These could be  a number, a word, boolen, an integer etc
+#  examples
+#  integers are referred to as int(positive and negative whole numbers)
+#  a float is a decimal number( not whole numbers  eg 1.2)
+#  strings ( a sequence of characters in  double quotes)
