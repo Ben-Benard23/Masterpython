573ba46addcfff32824ec29ae9d077b430a92922
“[Benard <ngwabern@gmail.com>
2024-10-19 14:11:10 -0500

    added project

---
+++ Day_3/exercise.py
@@ -0,0 +1,33 @@
+
+# rate = input("Please provide your rate: ")
+# rate = float(rate)
+# hours = input("please enter hours worked: ")
+
+
+# if hours >40:
+#     over_time_hours = hours- 40
+#     over_time_rate = 1.5*rate
+#     over_time_pay = over_time_hours*over_time_rate
+    
+while True:
+    try:   
+        rate = input("Please provide your rate: ")
+        rate = float(rate)
+        break
+    except ValueError:
+        print("Invalid entry")
+        print("Please try again")
+        
+while True:
+    try:   
+       hours = input("Please provide your hours: ")
+       hours = float(hours)
+       break
+    except ValueError:
+        print("Invalid entry")
+        print("please try again")
+        
+over_time_hours = hours - 40
+over_time_rate = 1.5*rate
+over_time_pay = over_time_hours*over_time_rate
+print(over_time_pay)
